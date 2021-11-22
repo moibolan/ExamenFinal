36e5255fea0184f209fe5614329ca6081f4f31b1
Moisés Bolaños Fuentes <moises.fuentes@hotmail.es>
2021-11-21 18:48:33 -0600

    Initial commit

---
+++ src/main/java/com/progra/api/rest/examenfinal/ProductApplication.java
@@ -0,0 +1,9 @@
+package com.progra.api.rest.examenfinal;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class ProductApplication extends Application {
+
+}
